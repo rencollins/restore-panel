42a57cee7d8171344c17bef58ad802259956880b
salomon <153840229+salomonorg@users.noreply.github.com>
2025-07-14 15:06:53 +0700

    Update repair.sh

--- repair.sh
+++ repair.sh
@@ -6,36 +6,39 @@ if (( $EUID != 0 )); then
 fi
 
 echo "SIMPLE TOOLS PTRODACTYLE"
-echo "This tool will help you repair your Pterodactyl panel."
 echo ""
-echo "Tools:"
+echo "Select a tool:"
 echo "1. Repair Panel"
+echo "2. Exit"
 echo ""
 
-repairPanel() {
-    echo "Starting panel repair..."
-    cd /var/www/pterodactyl || { echo "Failed to navigate to /var/www/pterodactyl"; exit 1; }
+read -p "Enter your choice [1/2]: " choice
 
-    php artisan down
-    rm -rf /var/www/pterodactyl/resources
-    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
-    chmod -R 755 storage/* bootstrap/cache
-    composer install --no-dev --optimize-autoloader
-    php artisan view:clear
-    php artisan config:clear
-    php artisan migrate --seed --force
-    chown -R www-data:www-data /var/www/pterodactyl/*
-    php artisan queue:restart
-    php artisan up
+case $choice in
+    1)
+        echo "Starting panel repair..."
+        cd /var/www/pterodactyl || { echo "Failed to navigate to /var/www/pterodactyl"; exit 1; }
 
-    echo "Panel repair completed successfully."
-}
+        php artisan down
+        rm -rf /var/www/pterodactyl/resources
+        curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+        chmod -R 755 storage/* bootstrap/cache
+        composer install --no-dev --optimize-autoloader
+        php artisan view:clear
+        php artisan config:clear
+        php artisan migrate --seed --force
+        chown -R www-data:www-data /var/www/pterodactyl/*
+        php artisan queue:restart
+        php artisan up
 
-while true; do
-    read -p "Do you want to run 'Repair Panel'? [y/n]: " yn
-    case $yn in
-        [Yy]* ) repairPanel; break;;
-        [Nn]* ) echo "Operation cancelled."; exit;;
-        * ) echo "Please answer y or n.";;
-    esac
-done
+        echo "Panel repair completed successfully."
+        ;;
+    2)
+        echo "Exiting..."
+        exit 0
+        ;;
+    *)
+        echo "Invalid option. Please run the script again and choose 1 or 2."
+        exit 1
+        ;;
+esac
